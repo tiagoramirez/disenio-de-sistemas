025a752727e9b85809cf18bf94e6b5dcaa3c6d66
Tiago Ramirez <tiagoramirez2001@gmail.com>
2023-09-27 02:15:00 -0300

    Agregado buscar por nombre, todas, modificar y eliminar

--- src/main/java/utn/frba/ejercicios_propuestos/pro_services/dominio/main/Main.java
+++ src/main/java/utn/frba/ejercicios_propuestos/pro_services/dominio/main/Main.java
@@ -1,5 +1,7 @@
 package utn.frba.ejercicios_propuestos.pro_services.dominio.main;
 
+import java.util.List;
+
 import javax.persistence.EntityTransaction;
 
 import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
@@ -9,8 +11,12 @@ public class Main implements WithSimplePersistenceUnit {
     public static void main(String[] args) {
         Main main = new Main();
         main.start();
-        Tarea tareaEncontrada = main.buscarTareaPorId(Long.parseLong("2"));
-        System.out.println(tareaEncontrada);
+        List<Tarea> todasLasTareas = main.buscarTodasTareas();
+        System.out.println(todasLasTareas);
+        Tarea tareaEncontrada2 = main.buscarTareaPorId(Long.parseLong("2"));
+        Tarea tareaEncontrada1 = main.buscarTareaPorNombre("Programacion");
+        main.modificarDescripcionTarea(tareaEncontrada1, tareaEncontrada1.getDescripcion() + "!!!!!");
+        main.eliminarTarea(tareaEncontrada2);
     }
 
     private void start() {
@@ -25,7 +31,39 @@ public class Main implements WithSimplePersistenceUnit {
 
     private Tarea buscarTareaPorId(Long id) {
         Tarea tareaEncontrada = entityManager().find(Tarea.class, id);
+        System.out.println(tareaEncontrada);
         return tareaEncontrada;
     }
 
+    private Tarea buscarTareaPorNombre(String nombreABuscar) {
+        Tarea tareaEncontrada = (Tarea) entityManager()
+                .createQuery("from " + Tarea.class.getName() + " where nombre = :nombre")
+                .setParameter("nombre", nombreABuscar)
+                .getSingleResult();
+        System.out.println(tareaEncontrada);
+        return tareaEncontrada;
+    }
+
+    private List<Tarea> buscarTodasTareas() {
+        List<Tarea> tareasEncontradas = entityManager()
+                .createQuery("from " + Tarea.class.getName())
+                .getResultList();
+        System.out.println(tareasEncontradas);
+        return tareasEncontradas;
+    }
+
+    private void modificarDescripcionTarea(Tarea tarea, String descripcion) {
+        tarea.setDescripcion(descripcion);
+        EntityTransaction tx = entityManager().getTransaction();
+        tx.begin();
+        entityManager().merge(tarea);
+        tx.commit();
+    }
+
+    private void eliminarTarea(Tarea tarea) {
+        EntityTransaction tx = entityManager().getTransaction();
+        tx.begin();
+        entityManager().remove(tarea);
+        tx.commit();
+    }
 }
